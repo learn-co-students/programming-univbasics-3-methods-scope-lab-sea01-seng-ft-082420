6df94a479ea15d1a82b0ae68f5b3a741718fc961
Luke Hatcher <lukehatcher98@gmail.com>
2020-07-05 21:35:09 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,15 +1,15 @@
-def mario
+def mario()
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-def toadstool
+def toadstool()
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
-def link
+def link()
   puts "It's Dangerous To Go Alone! Take This."
 end
 
